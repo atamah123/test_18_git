c31c0f91f6aa02e76e3e65413bf4a9dccd51b71c
atamah123 <atamah123@gmail.com>
2024-11-12 12:02:17 +0300

    Update Main.java

--- src/main/java/com/google/atamah123/Main.java
+++ src/main/java/com/google/atamah123/Main.java
@@ -2,6 +2,6 @@ package com.google.atamah123;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa-guru!");
+        System.out.println("Hello qa-guru!!!!!");
     }
 }
